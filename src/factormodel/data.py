c52f0c530f4d7beda34f95bcbbe5389af2f0e7f2
Lukas2k4 <lukasbos22@gmail.com>
2025-11-23 20:04:14 -0500

    yfinance data pulls

--- src/factormodel/data.py
+++ src/factormodel/data.py
@@ -2,4 +2,46 @@
 Tools for pulling and processing data used for factor modeling.
 """
 
-import yfinance
+import pandas as pd
+import yfinance as yf
+
+from utils import get_logger
+
+logger = get_logger(__name__)
+
+def fetch_raw_data(tickers: str | list, interval: str = '1d', start: str = None, end: str = None) -> pd.DataFrame:
+    """
+    Pull data from Yahoo! Finance's API
+
+    Args:
+        tickers (str | list): List of tickers to download if multiple tickers. Else the ticker as a string.
+        start (str): Start date of data, formatted 'yyyy-mm-dd'
+        end (str): End date of data, formatted' yyyy-mm-dd'
+        interval (str): Time between data points, eg '1m', '5m', '1d'.
+    
+    Returns:
+        pd.DataFrame: OHLC and volume data
+    """
+    data = yf.download(tickers=tickers, interval=interval, start=start, end=end)
+    
+    if _verify_yf_data(data):
+        return data
+    
+    return pd.DataFrame()
+    
+def clean_data():
+    pass
+
+def _verify_yf_data(data: pd.DataFrame) -> bool:
+
+    if not data.equals(data.dropna(inplace=False)):
+        logger.warning("NaN rows detected in YFinance data.")
+    
+    if data.empty:
+        logger.error("YFinance data is empty.")
+        return False
+    
+    return True
+
+if __name__ == "__main__":
+    print(fetch_raw_data(['TSLA', 'CRWV'], start='2022-01-01'))
---
+++ src/factormodel/factor_model.py
@@ -0,0 +1,9 @@
+"""
+Determine factor returns of the following five factors:
+
+1. Value
+2. Momentum
+3. Size
+4. Quality
+5. Low Volatility
+"""
